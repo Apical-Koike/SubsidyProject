c2fc3d81d3ae8777dc0c420dede370476bd8a715
Ay-Koike <39095525+Ay-Koike@users.noreply.github.com>
2020-10-24 19:38:33 +0900

    Hello Worldの追加・起動確認済み

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from hello import helloworld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.ursls),
+    url(r'^hello/', helloworld.hello_world),
 ]
+
+
